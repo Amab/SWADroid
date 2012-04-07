b928a5e499931902287b8151f421d3467e313a61
Juan Miguel Boyero Corral <juanmi1982@gmail.com>
2012-04-07 14:57:01 +0000

    Added automatic syncronization of notifications
    git-svn-id: https://forja.rediris.es/svn/cusl6-swadroid/trunk@261 5bc14d19-1e4b-4ba2-aa50-860af135f48c

--- SWADroid/gen/es/ugr/swad/swadroid/R.java
+++ SWADroid/gen/es/ugr/swad/swadroid/R.java
@@ -278,7 +278,11 @@ public final class R {
         public static final int yesMsg=0x7f07003b;
     }
     public static final class xml {
-        public static final int preferences=0x7f040000;
-        public static final int tables=0x7f040001;
+        public static final int account_preferences=0x7f040000;
+        public static final int authenticator=0x7f040001;
+        public static final int preferences=0x7f040002;
+        public static final int sync_notifications=0x7f040003;
+        public static final int syncadapter=0x7f040004;
+        public static final int tables=0x7f040005;
     }
 }
