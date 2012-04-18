f2f6b962911253d6b0706a72971cb00c9838cfa5
aguilerin <aguilerin@5bc14d19-1e4b-4ba2-aa50-860af135f48c>
2012-04-18 11:42:15 +0000

    Integrated ZXing into SWADroid project
    git-svn-id: https://forja.rediris.es/svn/cusl6-swadroid/trunk@286 5bc14d19-1e4b-4ba2-aa50-860af135f48c

--- SWADroid/src/es/ugr/swad/swadroid/modules/attendance/AttendanceConfigDownload.java
+++ SWADroid/src/es/ugr/swad/swadroid/modules/attendance/AttendanceConfigDownload.java
@@ -20,7 +20,6 @@ package es.ugr.swad.swadroid.modules.attendance;
 
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
-import java.util.List;
 import java.util.Vector;
 
 import org.ksoap2.SoapFault;
@@ -28,21 +27,14 @@ import org.ksoap2.serialization.SoapObject;
 import org.xmlpull.v1.XmlPullParserException;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 import es.ugr.swad.swadroid.Global;
 import es.ugr.swad.swadroid.Preferences;
 import es.ugr.swad.swadroid.R;
-import es.ugr.swad.swadroid.model.Course;
-import es.ugr.swad.swadroid.model.Model;
 import es.ugr.swad.swadroid.model.User;
-import es.ugr.swad.swadroid.modules.Courses;
 import es.ugr.swad.swadroid.modules.Module;
 
 /**
@@ -50,18 +42,6 @@ import es.ugr.swad.swadroid.modules.Module;
  * @author Antonio Aguilera Malagon <aguilerin@gmail.com>
  */
 public class AttendanceConfigDownload extends Module {
-	/**
-	 * Cursor for database access
-	 */
-	private Cursor dbCursor;
-	/**
-	 * User courses list
-	 */
-	private List<Model>listCourses;
-	/**
-	 * Selected course code
-	 */
-	private long selectedCourseCode = 0;
 	/**
 	 * Users counter
 	 */
@@ -85,12 +65,29 @@ public class AttendanceConfigDownload extends Module {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {		
 		super.onCreate(savedInstanceState);
-
-		prefs.getPreferences(getBaseContext());
-		Intent activity = new Intent(getBaseContext(), Courses.class);
-		Toast.makeText(getBaseContext(), R.string.coursesProgressDescription, Toast.LENGTH_LONG).show();
-		startActivityForResult(activity, Global.COURSES_REQUEST_CODE);
 		setMETHOD_NAME("getUsers");
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		prefs.getPreferences(getBaseContext());
+		try {					
+
+			if(isDebuggable) {
+				Log.d(TAG, "selectedCourseCode = " + Long.toString(Global.getSelectedCourseCode()));
+			}
+
+			runConnection();
+		} catch (Exception ex) {
+			String errorMsg = getString(R.string.errorServerResponseMsg);
+			error(errorMsg);
+
+			if(isDebuggable) {
+				Log.e(ex.getClass().getSimpleName(), errorMsg);        		
+				ex.printStackTrace();
+			}
+		}
 	}
 
 	/* (non-Javadoc)
@@ -98,69 +95,9 @@ public class AttendanceConfigDownload extends Module {
 	 */
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		int lastCourseSelected;
-
-		OnClickListener singleChoiceItemsClickListener = new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int whichButton) {
-				Course c = (Course) listCourses.get(whichButton);
-				selectedCourseCode = c.getId();
-				prefs.setLastCourseSelected(whichButton);
-
-				if(isDebuggable) {
-					Integer s = whichButton;
-					Log.d(TAG, "singleChoice = " + s.toString());
-				}
-			}
-		};
-
-		OnClickListener positiveClickListener = new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int whichButton) {
-				try {					
-					if(selectedCourseCode == 0) {
-						//Toast.makeText(getBaseContext(), R.string.noCourseSelectedMsg, Toast.LENGTH_LONG).show();
-						Course c = (Course) listCourses.get(0);
-						selectedCourseCode = c.getId();
-					}
-
-					if(isDebuggable) {
-						Log.d(TAG, "selectedCourseCode = " + Long.toString(selectedCourseCode));
-					}
-					runConnection();
-
-				} catch (Exception ex) {
-					String errorMsg = getString(R.string.errorServerResponseMsg);
-					error(errorMsg);
-
-					if(isDebuggable) {
-						Log.e(ex.getClass().getSimpleName(), errorMsg);        		
-						ex.printStackTrace();
-					}
-				}
-			}
-		};
-
-		OnClickListener negativeClickListener = new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int whichButton) {
-				dialog.cancel();
-				finish();
-			}
-		};
-
 		super.onActivityResult(requestCode, resultCode, intent);
 		if (resultCode == Activity.RESULT_OK) {
 			switch(requestCode) {
-			case Global.COURSES_REQUEST_CODE:
-				final AlertDialog.Builder alert = new AlertDialog.Builder(this);
-				dbCursor = dbHelper.getDb().getCursor(Global.DB_TABLE_COURSES);
-				listCourses = dbHelper.getAllRows(Global.DB_TABLE_COURSES);
-				lastCourseSelected = prefs.getLastCourseSelected();
-
-				alert.setSingleChoiceItems(dbCursor, lastCourseSelected, "name", singleChoiceItemsClickListener)
-				.setTitle(R.string.selectCourseTitle)
-				.setPositiveButton(R.string.acceptMsg, positiveClickListener)
-				.setNegativeButton(R.string.cancelMsg, negativeClickListener)
-				.show();
-				break;
 			case Global.SCAN_QR_REQUEST_CODE:
 				// Propagate the scan result to the previous activity
 				setResult(resultCode, intent);
@@ -184,7 +121,7 @@ public class AttendanceConfigDownload extends Module {
 		//Creates webservice request, adds required params and sends request to webservice
 		createRequest();
 		addParam("wsKey", Global.getLoggedUser().getWsKey());
-		addParam("courseCode", (int) selectedCourseCode);
+		addParam("courseCode", (int) Global.getSelectedCourseCode());
 		addParam("groupCode", 0);		// All groups
 		addParam("userRole", userRole);
 		sendRequest(User.class, false);
@@ -224,7 +161,7 @@ public class AttendanceConfigDownload extends Module {
 				if (dbHelper.insertUser(u))
 					insertedUsersCount++;
 
-				dbHelper.insertUserCourse(u, selectedCourseCode);
+				dbHelper.insertUserCourse(u, Global.getSelectedCourseCode());
 			}	// end for (int i=0; i < usersCount; i++)
 
 			//Request finalized without errors
@@ -255,17 +192,11 @@ public class AttendanceConfigDownload extends Module {
 	protected void postConnect() {
 		Log.i(TAG, "Added " + insertedUsersCount + " new users");
 		Toast.makeText(this, "La asignatura seleccionada tiene " + usersCount + " estudiantes", Toast.LENGTH_LONG).show();
-		//finish();
 
 		Intent activity = new Intent("es.ugr.swad.swadroid.android.SCAN");
 		activity.putExtra("SCAN_MODE", "QR_CODE_MODE");
 		activity.putExtra("SCAN_FORMATS", "QR_CODE");
-		activity.putExtra("selectedCourseCode", selectedCourseCode);
-		// pasar a CaptureActivity selectedCourseCode + cualquier otra informacion que necesite
-		// recibir esta informacion adecuadamente en CaptureActivity y trabajar a partir de ella (ej: lista 
-		//de alumnos -> comprobar al escanear un QR si pertenece a la lista)
 
-		//activity.putExtra("selectedCourseCode", selectedCourseCode);
 		startActivityForResult(activity, Global.SCAN_QR_REQUEST_CODE);
 	}
 
