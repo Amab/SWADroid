ddc540e1ed38e1abb8c0f9e16a5d2083e147b566
Juan Miguel Boyero Corral <juanmi1982@gmail.com>
2012-05-13 19:12:55 +0200

    Fixed closed cursor access exception on Android 4.x platforms

--- SWADroid/src/es/ugr/swad/swadroid/modules/notifications/Notifications.java
+++ SWADroid/src/es/ugr/swad/swadroid/modules/notifications/Notifications.java
@@ -114,8 +114,9 @@ public class Notifications extends Module {
 	/**
 	 * Refreshes data on screen
 	 */
-	private void refreshScreen() {
+	private void refreshScreen() {		
 		//Refresh data on screen 
+		stopManagingCursor(dbCursor);
 		dbCursor = dbHelper.getDb().getCursor(Global.DB_TABLE_NOTIFICATIONS, selection, orderby);
 		startManagingCursor(dbCursor);
 		adapter.changeCursor(dbCursor);
@@ -220,7 +221,7 @@ public class Notifications extends Module {
 	@Override
 	protected void onResume() {		
 		super.onResume();
-		
+
 		IntentFilter intentFilter = new IntentFilter();
 	    intentFilter.addAction(NotificationsSyncAdapterService.START_SYNC);
 	    intentFilter.addAction(NotificationsSyncAdapterService.STOP_SYNC);
