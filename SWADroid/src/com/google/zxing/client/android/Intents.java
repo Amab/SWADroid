080617d8e61f26db1ef10ddb949232e9beee7e3b
Amab <juanmi1982@gmail.com>
2013-03-26 21:11:43 +0100

    Fixed NullPointerException exception on QR code decoding.

--- SWADroid/src/com/google/zxing/client/android/Intents.java
+++ SWADroid/src/com/google/zxing/client/android/Intents.java
@@ -31,7 +31,7 @@ public final class Intents {
      * Send this intent to open the Barcodes app in scanning mode, find a barcode, and return
      * the results.
      */
-    public static final String ACTION = "com.google.zxing.client.android.SCAN";
+    public static final String ACTION = "es.ugr.swad.swadroid.android.SCAN";
 
     /**
      * By default, sending this will decode all barcodes that we understand. However it
