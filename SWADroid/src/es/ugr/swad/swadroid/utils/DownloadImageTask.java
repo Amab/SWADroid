c3f09dca0f74cbe417be90f7f84419c7f946f114
Juan Miguel Boyero Corral <juanmi1982@gmail.com>
2013-04-27 02:30:18 +0200

    Removed BugSense reports

--- SWADroid/src/es/ugr/swad/swadroid/utils/DownloadImageTask.java
+++ SWADroid/src/es/ugr/swad/swadroid/utils/DownloadImageTask.java
@@ -2,8 +2,6 @@ package es.ugr.swad.swadroid.utils;
 
 import java.io.InputStream;
 
-import com.bugsense.trace.BugSenseHandler;
-
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
@@ -29,9 +27,6 @@ public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
         } catch (Exception e) {
             Log.e("DownloadImageTask", e.getMessage());
             e.printStackTrace();
-        	
-			//Send exception details to Bugsense
-			BugSenseHandler.sendException(e);
         }
         return mIcon11;
     }
