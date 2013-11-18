49002b6ed480991e3c0dda5b5d56e642db7357ce
Jose Antonio Guerrero Aviles <antonioguerreroaviles@gmail.com>
2013-11-18 14:40:09 +0100

    Added new layout for information modules

--- SWADroid/src/es/ugr/swad/swadroid/modules/information/Introduction.java
+++ SWADroid/src/es/ugr/swad/swadroid/modules/information/Introduction.java
@@ -9,11 +9,26 @@ package es.ugr.swad.swadroid.modules.information;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import org.xmlpull.v1.XmlPullParserException;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import es.ugr.swad.swadroid.R;
 import es.ugr.swad.swadroid.modules.Module;
 
 
 public class Introduction extends Module {
 
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+		
+		WebView webview;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview_information_screen_layout);
+        webview = (WebView) this.findViewById(R.id.contentWebView);
+       
+    }
+    
+    
 	@Override
 	protected void requestService() throws NoSuchAlgorithmException,
 			IOException, XmlPullParserException {
