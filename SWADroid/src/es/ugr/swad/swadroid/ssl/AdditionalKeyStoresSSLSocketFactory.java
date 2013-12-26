5f472f33e9d0ce6a13b04bf1415ecb9aa6b63c77
Juan Miguel Boyero Corral <juanmi1982@gmail.com>
2013-12-26 11:54:25 +0100

    Added header comments

--- SWADroid/src/es/ugr/swad/swadroid/ssl/AdditionalKeyStoresSSLSocketFactory.java
+++ SWADroid/src/es/ugr/swad/swadroid/ssl/AdditionalKeyStoresSSLSocketFactory.java
@@ -1,3 +1,21 @@
+/*
+ *  This file is part of SWADroid.
+ *
+ *  Copyright (C) 2010 Juan Miguel Boyero Corral <juanmi1982@gmail.com>
+ *
+ *  SWADroid is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  SWADroid is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with SWADroid.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package es.ugr.swad.swadroid.ssl;
 
 import java.io.IOException;
@@ -23,6 +41,8 @@ import javax.net.ssl.X509TrustManager;
 /**
  * Allows you to trust certificates from additional KeyStores in addition to
  * the default KeyStore
+ * 
+ * @author Juan Miguel Boyero Corral <juanmi1982@gmail.com>
  */
 public class AdditionalKeyStoresSSLSocketFactory extends SSLSocketFactory {
     protected SSLContext sslContext = SSLContext.getInstance("TLS");
