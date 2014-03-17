d5d0a0c695c70568171057bd9dccbdc83844f074
Jose Antonio Guerrero Aviles <antonioguerreroaviles@gmail.com>
2014-03-17 13:29:14 +0100

    Changed information module order. Updated icons.

--- SWADroid/src/es/ugr/swad/swadroid/modules/notifications/NotificationItem.java
+++ SWADroid/src/es/ugr/swad/swadroid/modules/notifications/NotificationItem.java
@@ -68,7 +68,7 @@ public class NotificationItem extends MenuActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.single_notification_view);
 
-    	getSupportActionBar().setIcon(R.drawable.notif);
+    	getSupportActionBar().setIcon(R.drawable.notif_black);
 
         senderTextView = (TextView) this.findViewById(R.id.senderNameText);
         courseTextView = (TextView) this.findViewById(R.id.courseNameText);
