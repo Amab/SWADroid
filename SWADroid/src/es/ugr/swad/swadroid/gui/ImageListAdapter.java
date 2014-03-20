0c36ae65e5f23104019e655beef3cf71bae18bed
Amab <juanmi1982@gmail.com>
2014-03-21 00:26:51 +0100

    Added images to Tests main menu

---
+++ SWADroid/src/es/ugr/swad/swadroid/gui/ImageListAdapter.java
@@ -0,0 +1,51 @@
+package es.ugr.swad.swadroid.gui;
+
+import java.util.List;
+
+import es.ugr.swad.swadroid.R;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+ 
+public class ImageListAdapter extends ArrayAdapter<ImageListItem> {
+ 
+    Context context;
+ 
+    public ImageListAdapter(Context context, int resourceId,
+            List<ImageListItem> items) {
+        super(context, resourceId, items);
+        this.context = context;
+    }
+ 
+    /*private view holder class*/
+    private class ViewHolder {
+        ImageView imageView;
+        TextView txtTitle;
+    }
+ 
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        ImageListItem imageListItem = getItem(position);
+ 
+        LayoutInflater mInflater = (LayoutInflater) context
+                .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.list_image_item, null);
+            holder = new ViewHolder();
+            holder.txtTitle = (TextView) convertView.findViewById(R.id.TextView1);
+            holder.imageView = (ImageView) convertView.findViewById(R.id.imageView1);
+            convertView.setTag(holder);
+        } else
+            holder = (ViewHolder) convertView.getTag();
+
+        holder.txtTitle.setText(imageListItem.getTitle());
+        holder.imageView.setImageResource(imageListItem.getImageId());
+ 
+        return convertView;
+    }
+}
