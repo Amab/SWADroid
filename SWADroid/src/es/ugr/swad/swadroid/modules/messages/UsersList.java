bdfbe30bd4d453a6c0a729fd954f3d8880d43a22
Jose Antonio Guerrero Aviles <antonioguerreroaviles@gmail.com>
2014-07-24 17:07:47 +0100

    Now messages show the users list.

--- SWADroid/src/es/ugr/swad/swadroid/modules/messages/UsersList.java
+++ SWADroid/src/es/ugr/swad/swadroid/modules/messages/UsersList.java
@@ -6,20 +6,32 @@
 
 package es.ugr.swad.swadroid.modules.messages;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 import es.ugr.swad.swadroid.Constants;
 import es.ugr.swad.swadroid.R;
 import es.ugr.swad.swadroid.gui.MenuActivity;
+import es.ugr.swad.swadroid.model.User;
 import es.ugr.swad.swadroid.modules.rollcall.RollcallConfigDownload;
+import es.ugr.swad.swadroid.modules.rollcall.students.StudentItemModel;
+import es.ugr.swad.swadroid.modules.rollcall.students.StudentsArrayAdapter;
 
 public class UsersList extends MenuActivity {
 
 	public static final String TAG = Constants.APP_TAG + " Users List";
+    private List<StudentItemModel> studentsList;
+    
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +39,7 @@ public class UsersList extends MenuActivity {
 		setContentView(R.layout.users_listview);
 		setTitle(R.string.selectRcvModuleLabel);
 		getSupportActionBar().setIcon(R.drawable.users);
-	
+	        
 		Button cancelList = (Button) findViewById(R.id.cancelList);
 		cancelList.setOnClickListener(new View.OnClickListener() {
 			
@@ -50,6 +62,15 @@ public class UsersList extends MenuActivity {
     @Override
     protected void onStart() {
         super.onStart();
+		//TODO cargar la lista de usuarios que hay en la memoria del teléfono
+		/*La primera vez deberá cargar una lista vacía y para obtener la primera lista, 
+		 * habrá que pulsar en el botón actualizar de la actionbar. A partir de este momento,
+		 * tendremos una lista de usuarios en la memoria del teléfono y cuando  entre aquí la segunda
+		 * vez y sucesivas, será esa la lista que se cargue. Si queremos, podemos volver a pulsar el
+		 * botón actualizar por si hay nuevos usuarios.*/
+
+        	showStudentsList();
+	
         /*try {
             runConnection();
         } catch (Exception e) {
@@ -80,6 +101,29 @@ public class UsersList extends MenuActivity {
 		
 	}
 	
+	 private void showStudentsList() {
+	        List<Long> idList = dbHelper.getUsersCourse(Constants.getSelectedCourseCode());
+	        if (!idList.isEmpty()) {
+	            studentsList = new ArrayList<StudentItemModel>();
+
+	            for (Long userCode : idList) {
+	                User u = dbHelper.getUser("userCode", String.valueOf(userCode));
+	                studentsList.add(new StudentItemModel(u));
+	            }
+	            // Arrange the list alphabetically
+	            Collections.sort(studentsList);
+	            
+	            // Show the list of students in the ListView
+	            ListView lv = (ListView) findViewById(R.id.users_listview);
+	            lv.setAdapter(new StudentsArrayAdapter(this, studentsList, Constants.ROLLCALL_REQUEST_CODE));
+
+	            
+	        } else {
+	            Toast.makeText(this, R.string.scan_no_students, Toast.LENGTH_LONG).show();
+	        }
+	    }
+	 
+	
 	 @Override
 		public boolean onCreateOptionsMenu(Menu menu) {
 		    getMenuInflater().inflate(R.menu.users_list_activity_actions, menu);
@@ -98,9 +142,13 @@ public class UsersList extends MenuActivity {
 	            
 	        //Refresh users list    
 	        case R.id.action_refresh_users:
-	        	
-				Intent refreshUsersList = new Intent (getBaseContext(), RollcallConfigDownload.class);
-				startActivity(refreshUsersList);
+	        					
+				Intent refreshUsersList;
+				Context context = getApplicationContext();
+				
+				refreshUsersList = new Intent(context, RollcallConfigDownload.class);
+				refreshUsersList.putExtra("groupCode", (long) 0);
+		        startActivity(refreshUsersList);
 				
 	            return true;
 	
