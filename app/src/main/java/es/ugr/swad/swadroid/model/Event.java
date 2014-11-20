f95aa2a392004d927bbeede5887084339fe6c33e
Juan Miguel Boyero Corral <juanmi1982@gmail.com>
2014-11-20 21:14:53 +0100

    Removed sorting of Events and Users

--- app/src/main/java/es/ugr/swad/swadroid/model/Event.java
+++ app/src/main/java/es/ugr/swad/swadroid/model/Event.java
@@ -35,7 +35,7 @@ import java.util.List;
  *
  * @author Juan Miguel Boyero Corral <juanmi1982@gmail.com>
  */
-public class Event extends Model implements Comparable<Event> {
+public class Event extends Model {
     private boolean hidden;
     private String userSurname1;
     private String userSurname2;
@@ -356,50 +356,6 @@ public class Event extends Model implements Comparable<Event> {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Event)) return false;
-        if (!super.equals(o)) return false;
-
-        Event event = (Event) o;
-
-        if (commentsTeachersVisible != event.commentsTeachersVisible) return false;
-        if (endTime != event.endTime) return false;
-        if (hidden != event.hidden) return false;
-        if (startTime != event.startTime) return false;
-        if (groups != null ? !groups.equals(event.groups) : event.groups != null) return false;
-        if (text != null ? !text.equals(event.text) : event.text != null) return false;
-        if (title != null ? !title.equals(event.title) : event.title != null) return false;
-        if (userFirstName != null ? !userFirstName.equals(event.userFirstName) : event.userFirstName != null)
-            return false;
-        if (userPhoto != null ? !userPhoto.equals(event.userPhoto) : event.userPhoto != null)
-            return false;
-        if (userSurname1 != null ? !userSurname1.equals(event.userSurname1) : event.userSurname1 != null)
-            return false;
-        if (userSurname2 != null ? !userSurname2.equals(event.userSurname2) : event.userSurname2 != null)
-            return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + (hidden ? 1 : 0);
-        result = 31 * result + (userSurname1 != null ? userSurname1.hashCode() : 0);
-        result = 31 * result + (userSurname2 != null ? userSurname2.hashCode() : 0);
-        result = 31 * result + (userFirstName != null ? userFirstName.hashCode() : 0);
-        result = 31 * result + (userPhoto != null ? userPhoto.hashCode() : 0);
-        result = 31 * result + startTime;
-        result = 31 * result + endTime;
-        result = 31 * result + (commentsTeachersVisible ? 1 : 0);
-        result = 31 * result + (title != null ? title.hashCode() : 0);
-        result = 31 * result + (text != null ? text.hashCode() : 0);
-        result = 31 * result + (groups != null ? groups.hashCode() : 0);
-        return result;
-    }
-
-    @Override
     public String toString() {
         return "Event{" +
                 "eventCode='" + super.getId() + '\'' +
@@ -415,10 +371,5 @@ public class Event extends Model implements Comparable<Event> {
                 ", text='" + text + '\'' +
                 ", groups='" + groups + '\'' +
                 '}';
-    }
-
-    @Override
-    public int compareTo(Event another) {
-        return this.getTitle().compareToIgnoreCase(another.getTitle());
     }
 }
