245bf5007fb9913a8130a3182df360239eb217b9
Amab <juanmi1982@gmail.com>
2014-11-26 22:50:11 +0100

    Fixed refresh bugs in lists of users and attendances

--- app/src/main/java/es/ugr/swad/swadroid/modules/rollcall/UsersCursorAdapter.java
+++ app/src/main/java/es/ugr/swad/swadroid/modules/rollcall/UsersCursorAdapter.java
@@ -22,7 +22,6 @@ package es.ugr.swad.swadroid.modules.rollcall;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -51,6 +50,15 @@ public class UsersCursorAdapter extends CursorAdapter {
     private Crypto crypto;
     private ImageLoader loader;
     private int eventCode;
+    private LayoutInflater inflater;
+
+    private static class ViewHolder {
+        ImageView image;
+        TextView text1;
+        TextView text2;
+        CheckBox checkbox;
+
+    }
 
     /**
      * Constructor
@@ -67,8 +75,9 @@ public class UsersCursorAdapter extends CursorAdapter {
         this.dbHelper = dbHelper;
         this.eventCode = eventCode;
         this.crypto = new Crypto(context, dbHelper.getDBKey());
-        this.loader = ImageFactory.init(context, true, true, R.raw.usr_bl, R.raw.usr_bl,
-                R.raw.usr_bl);
+        this.loader = ImageFactory.init(context, true, true, R.drawable.usr_bl, R.drawable.usr_bl,
+                R.drawable.usr_bl);
+        this.inflater = LayoutInflater.from(context);
     }
 
     /**
@@ -87,8 +96,9 @@ public class UsersCursorAdapter extends CursorAdapter {
 
         this.dbHelper = dbHelper;
         this.eventCode = eventCode;
-        this.loader = ImageFactory.init(context, true, true, R.raw.usr_bl, R.raw.usr_bl,
-                R.raw.usr_bl);
+        this.loader = ImageFactory.init(context, true, true, R.drawable.usr_bl, R.drawable.usr_bl,
+                R.drawable.usr_bl);
+        this.inflater = LayoutInflater.from(context);
     }
 
     @Override
@@ -101,38 +111,80 @@ public class UsersCursorAdapter extends CursorAdapter {
         String userPhoto = cursor.getString(cursor.getColumnIndex("photoPath"));
         boolean present = Utils.parseIntBool(cursor.getInt(cursor.getColumnIndex("present")));
 
-        ImageView image = (ImageView) view.findViewById(R.id.imageView1);
-        TextView text1 = (TextView) view.findViewById(R.id.TextView1);
-        TextView text2 = (TextView) view.findViewById(R.id.TextView2);
-        CheckBox checkbox = (CheckBox) view.findViewById(R.id.check);
+        final ViewHolder holder = (ViewHolder) view.getTag();
+        view.setTag(holder);
 
-        checkbox.setChecked(present);
-        checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        holder.image = (ImageView) view.findViewById(R.id.imageView1);
+        holder.text1 = (TextView) view.findViewById(R.id.TextView1);
+        holder.text2 = (TextView) view.findViewById(R.id.TextView2);
+        holder.checkbox = (CheckBox) view.findViewById(R.id.check);
+
+        holder.checkbox.setChecked(present);
+        holder.checkbox.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                //Inserts attendance into database
-                dbHelper.insertAttendance(userCode, eventCode, isChecked);
+            public void onClick(View v) {
+                Cursor oldCursor;
 
-                //Refresh ListView
-                dbCursor = dbHelper.getUsersEventCursor(eventCode);
-                changeCursor(dbCursor);
+                //Inserts attendance into database
+                dbHelper.insertAttendance(userCode, eventCode, holder.checkbox.isChecked());
 
                 //Mark event status as "pending"
                 dbHelper.updateEventStatus(eventCode, "pending");
+
+                //Refresh ListView
+                dbCursor = dbHelper.getUsersEventCursor(eventCode);
+                if (android.os.Build.VERSION.SDK_INT > android.os.Build.VERSION_CODES.GINGERBREAD) {
+                    oldCursor = swapCursor(dbCursor);
+                    oldCursor.close();
+                } else {
+                    changeCursor(dbCursor);
+                }
+                notifyDataSetChanged();
             }
         });
+        /*holder.checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Cursor oldCursor;
 
+                //Inserts attendance into database
+                dbHelper.insertAttendance(userCode, eventCode, isChecked);
+
+                //Mark event status as "pending"
+                dbHelper.updateEventStatus(eventCode, "pending");
+
+                //Refresh ListView
+                dbCursor = dbHelper.getUsersEventCursor(eventCode);
+                if (android.os.Build.VERSION.SDK_INT > android.os.Build.VERSION_CODES.GINGERBREAD) {
+                    oldCursor = swapCursor(dbCursor);
+                    oldCursor.close();
+                } else {
+                    changeCursor(dbCursor);
+                }
+                notifyDataSetChanged();
+            }
+        });*/
+
+        holder.image.setImageResource(R.drawable.usr_bl);
         if(userPhoto != null) {
-            ImageFactory.displayImage(loader, crypto.decrypt(userPhoto), image);
+            ImageFactory.displayImage(loader, crypto.decrypt(userPhoto), holder.image);
         }
 
-        text1.setText(userSurname1 + " " + userSurname2 + ", " + userFirstname);
-        text2.setText(userID);
+        holder.text1.setText(userSurname1 + " " + userSurname2 + ", " + userFirstname);
+        holder.text2.setText(userID);
     }
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        LayoutInflater vi = LayoutInflater.from(context);
-        return vi.inflate(R.layout.users_list_item, parent, false);
+        View view = inflater.inflate(R.layout.users_list_item, parent, false);
+        ViewHolder holder = new ViewHolder();
+
+        holder.image = (ImageView) view.findViewById(R.id.imageView1);
+        holder.text1 = (TextView) view.findViewById(R.id.TextView1);
+        holder.text2 = (TextView) view.findViewById(R.id.TextView2);
+        holder.checkbox = (CheckBox) view.findViewById(R.id.check);
+        view.setTag(holder);
+
+        return view;
     }
 }
