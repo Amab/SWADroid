402ea6a1264021a6f49dd34582b9e60633806584
Amab <juanmi1982@gmail.com>
2014-11-27 20:35:20 +0100

    Added sorting of events list

--- app/src/main/java/es/ugr/swad/swadroid/database/DataBaseHelper.java
+++ app/src/main/java/es/ugr/swad/swadroid/database/DataBaseHelper.java
@@ -677,7 +677,8 @@ public class DataBaseHelper {
     public Cursor getEventsCourseCursor(long crsCod) {
         return db.rawQuery("SELECT * FROM " + DB_TABLE_EVENTS_ATTENDANCES + " AS E"
                 + " INNER JOIN " + DB_TABLE_EVENTS_COURSES + " AS C"
-                + " ON E.id = C.eventCode WHERE C.crsCod ='" + crsCod + "'", null);
+                + " ON E.id = C.eventCode WHERE C.crsCod ='" + crsCod + "' AND hidden=" + Utils.parseBoolInt(false)
+                + " ORDER BY E.startTime DESC,E.endTime DESC,E.title DESC", null);
     }
 
     /**
