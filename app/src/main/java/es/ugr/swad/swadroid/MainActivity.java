46dd27f57180ec9d8bb804b799c226180599b515
Alejandro Alcalde <algui91@gmail.com>
2015-03-15 22:45:42 +0100

    Begining to implement material design

---
+++ app/src/main/java/es/ugr/swad/swadroid/MainActivity.java
@@ -0,0 +1,88 @@
+package es.ugr.swad.swadroid;
+
+/**
+ * @author Alejandro Alcalde (elbauldelprogramador.com) on 15/03/15.
+ */
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import es.ugr.swad.swadroid.gui.CourseAdapter;
+import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
+import it.neokree.materialnavigationdrawer.elements.MaterialSection;
+
+/**
+ * @author Alejandro Alcalde (elbauldelprogramador.com)
+ */
+public class MainActivity extends MaterialNavigationDrawer {
+
+    @Override
+    public void init(Bundle bundle) {
+        this.disableLearningPattern();
+
+        MaterialSection section1 = newSection(getString(R.string.course), R.drawable.crs, new CourseFragment());
+        MaterialSection section2 = newSection(getString(R.string.evaluation), R.drawable.ass, new CourseFragment());
+        MaterialSection section3 = newSection(getString(R.string.users), R.drawable.users, new CourseFragment());
+        MaterialSection section4 = newSection(getString(R.string.messages), R.drawable.msg, new CourseFragment());
+
+        addSection(section1);
+        addSection(section2);
+        addSection(section3);
+        addSection(section4);
+    }
+
+    public static class CourseFragment extends Fragment {
+
+        private static final String TAG = "CourseFragment";
+        private static final int DATASET_COUNT = 60;
+
+        private RecyclerView mRecyclerView;
+        private RecyclerView.Adapter mAdapter;
+        private RecyclerView.LayoutManager mLayoutManager;
+
+        protected String[] mDataset;
+
+        public CourseFragment() {
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            initDataset();
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.courses_fragment, container, false);
+            rootView.setTag(TAG);
+
+            mRecyclerView = (RecyclerView) rootView.findViewById(R.id.my_recycler_view);
+            mRecyclerView.setHasFixedSize(true);
+            mLayoutManager = new LinearLayoutManager(getActivity());
+            mRecyclerView.setLayoutManager(mLayoutManager);
+
+            mAdapter = new CourseAdapter(mDataset);
+            // Set CustomAdapter as the adapter for RecyclerView.
+            mRecyclerView.setAdapter(mAdapter);
+
+            return rootView;
+        }
+
+        /**
+         * Generates Strings for RecyclerView's adapter. This data would usually come
+         * from a local content provider or remote server.
+         */
+        private void initDataset() {
+            mDataset = new String[DATASET_COUNT];
+            for (int i = 0; i < DATASET_COUNT; i++) {
+                mDataset[i] = "This is element #" + i;
+            }
+        }
+    }
+}
---
+++ app/src/main/java/es/ugr/swad/swadroid/gui/CourseAdapter.java
@@ -0,0 +1,77 @@
+package es.ugr.swad.swadroid.gui;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import es.ugr.swad.swadroid.R;
+
+/**
+ * @author Alejandro Alcalde (elbauldelprogramador.com)
+ */
+public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.ViewHolder> {
+
+    private String[] mDataset;
+
+    /**
+     * Provide a reference to the type of views that you are using (custom ViewHolder)
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textView;
+
+        public ViewHolder(View v) {
+            super(v);
+            // Define click listener for the ViewHolder's View.
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d("CUSTOMADAPTER", "Element " + getPosition() + " clicked.");
+                }
+            });
+            textView = (TextView) v.findViewById(android.R.id.text1);
+            textView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.crs, 0, 0, 0);
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
+    }
+
+    /**
+     * Initialize the dataset of the Adapter.
+     *
+     * @param dataSet String[] containing the data to populate views to be used by RecyclerView.
+     */
+    public CourseAdapter(String[] dataSet) {
+        mDataset = dataSet;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public CourseAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        // Create a new view.
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(android.R.layout.simple_dropdown_item_1line, viewGroup, false);
+
+        return new ViewHolder(v);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(CourseAdapter.ViewHolder viewHolder, int position) {
+        Log.d("CUSTOM", "Element " + position + " set.");
+
+        // Get element from your dataset at this position and replace the contents of the view
+        // with that element
+        viewHolder.getTextView().setText(mDataset[position]);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
