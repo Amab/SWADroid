bb9fea000bcdb0e9244cf91f946c89a5cb631510
Alejandro Alcalde <algui91@gmail.com>
2015-03-20 11:53:28 +0100

    Added Account Headers

--- app/src/main/java/es/ugr/swad/swadroid/MainActivity.java
+++ app/src/main/java/es/ugr/swad/swadroid/MainActivity.java
@@ -5,9 +5,11 @@ package es.ugr.swad.swadroid;
  */
 
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,55 +20,66 @@ import es.ugr.swad.swadroid.gui.CourseAdapter;
 import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
 import it.neokree.materialnavigationdrawer.elements.MaterialAccount;
 import it.neokree.materialnavigationdrawer.elements.MaterialSection;
+import it.neokree.materialnavigationdrawer.elements.listeners.MaterialAccountListener;
 
 /**
  * @author Alejandro Alcalde (elbauldelprogramador.com)
  */
-public class MainActivity extends MaterialNavigationDrawer {
+public class MainActivity extends MaterialNavigationDrawer implements MaterialAccountListener {
 
     @Override
     public void init(Bundle bundle) {
         this.disableLearningPattern();
 
-
         MaterialAccount account1 = new MaterialAccount(
                 this.getResources(),
                 "LMD",
                 "Dep. Álgebra",
-                R.drawable.crs,
-                0x0000ff);
+                R.drawable.photo,
+                R.drawable.bamboo);
         MaterialAccount account2 = new MaterialAccount(
                 this.getResources(),
                 "CRIP",
                 "Dep. Álgebra",
-                R.drawable.ass,
-                0xff0000);
+                R.drawable.photo2,
+                R.drawable.mat2);
         MaterialAccount account3 = new MaterialAccount(
                 this.getResources(),
                 "MH",
                 "Decsai",
-                R.drawable.msg,
-                0x00ff00);
+                R.drawable.photo,
+                R.drawable.mat3);
 
-        setDrawerBackgroundColor(getResources().getColor(android.R.color.darker_gray));
-
-        this.addAccount(account1);
+        addAccount(account1);
         addAccount(account2);
         addAccount(account3);
 
-        MaterialSection section1 = newSection(getString(R.string.course), R.drawable.crs,
+        MaterialSection section1 = newSection(getString(R.string.course),
                 new CourseFragment());
         MaterialSection section2 = newSection(getString(R.string.evaluation), R.drawable.ass,
                 new CourseFragment());
         MaterialSection section3 = newSection(getString(R.string.users), R.drawable.users,
                 new CourseFragment());
         MaterialSection section4 = newSection(getString(R.string.messages), R.drawable.msg,
-                new CourseFragment());
+                new CourseFragment()).setSectionColor(Color.parseColor("#ff0000"));
 
         addSection(section1);
         addSection(section2);
         addSection(section3);
         addSection(section4);
+
+        setDrawerHeaderImage(R.drawable.mat3);
+        setAccountListener(this);
+    }
+
+    @Override
+    public void onAccountOpening(MaterialAccount materialAccount) {
+        Log.w(Constants.APP_TAG, "OnAccountOppening");
+    }
+
+    @Override
+    public void onChangeAccount(MaterialAccount materialAccount) {
+        Log.w(Constants.APP_TAG, "OnChangeAccount");
     }
 
     public static class CourseFragment extends Fragment {
