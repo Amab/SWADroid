5e043b8394670a2458fadf5463878207cfaa3ab6
Alejandro Alcalde <algui91@gmail.com>
2015-03-20 12:48:00 +0100

    Moving CourseFragment to a file on its own

--- app/src/main/java/es/ugr/swad/swadroid/MainActivity.java
+++ app/src/main/java/es/ugr/swad/swadroid/MainActivity.java
@@ -4,21 +4,13 @@ package es.ugr.swad.swadroid;
  * @author Alejandro Alcalde (elbauldelprogramador.com) on 15/03/15.
  */
 
-import android.app.Fragment;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import java.util.ArrayList;
 
 import es.ugr.swad.swadroid.database.DataBaseHelper;
-import es.ugr.swad.swadroid.gui.CourseAdapter;
+import es.ugr.swad.swadroid.gui.CourseFragment;
 import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
 import it.neokree.materialnavigationdrawer.elements.MaterialAccount;
 import it.neokree.materialnavigationdrawer.elements.MaterialSection;
@@ -82,9 +74,7 @@ public class MainActivity extends MaterialNavigationDrawer implements MaterialAc
 
         setDrawerHeaderImage(R.drawable.mat3);
         setAccountListener(this);
-        addBottomSection(newSection("Bottom Section",R.mipmap.ic_settings_black_24dp,new Intent(this,PreferencesActivity.class)));
-
-
+        addBottomSection(newSection("Bottom Section", R.mipmap.ic_settings_black_24dp, new Intent(this, PreferencesActivity.class)));
 
         try {
             //Initialize preferences
@@ -125,59 +115,6 @@ public class MainActivity extends MaterialNavigationDrawer implements MaterialAc
             dbHelper = new DataBaseHelper(this);
         } catch (Exception ex) {
             Log.e(Constants.APP_TAG, ex.getMessage());
-        }
-    }
-
-    public static class CourseFragment extends Fragment {
-
-        private static final String TAG = "CourseFragment";
-
-        private static final int DATASET_COUNT = 60;
-
-        protected ArrayList<String> mDataset;
-
-        private RecyclerView mRecyclerView;
-
-        private RecyclerView.Adapter mAdapter;
-
-        private RecyclerView.LayoutManager mLayoutManager;
-
-        public CourseFragment() {
-        }
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            initDataset();
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.courses_fragment, container, false);
-            rootView.setTag(TAG);
-
-            mRecyclerView = (RecyclerView) rootView.findViewById(R.id.my_recycler_view);
-            mRecyclerView.setHasFixedSize(true);
-            mLayoutManager = new LinearLayoutManager(getActivity());
-            mRecyclerView.setLayoutManager(mLayoutManager);
-
-            mAdapter = new CourseAdapter(mDataset);
-            // Set CustomAdapter as the adapter for RecyclerView.
-            mRecyclerView.setAdapter(mAdapter);
-
-            return rootView;
-        }
-
-        /**
-         * Generates Strings for RecyclerView's adapter. This data would usually come
-         * from a local content provider or remote server.
-         */
-        private void initDataset() {
-            mDataset = new ArrayList<>();
-            for (int i = 0; i < DATASET_COUNT; i++) {
-                mDataset.add("This is element #" + i);
-            }
         }
     }
 }
