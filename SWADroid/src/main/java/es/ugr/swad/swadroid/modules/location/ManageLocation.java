3ed6bd3acd4589732f33850b67cb339329e7f449
javibl8@correo.ugr.es <javibl8@correo.ugr.es>
2020-02-20 13:48:18 +0100

    Adding view layout to manage location

--- SWADroid/src/main/java/es/ugr/swad/swadroid/modules/location/ManageLocation.java
+++ SWADroid/src/main/java/es/ugr/swad/swadroid/modules/location/ManageLocation.java
@@ -1,4 +1,62 @@
 package es.ugr.swad.swadroid.modules.location;
 
-public class ManageLocation {
+import android.content.Intent;
+import android.os.Bundle;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import es.ugr.swad.swadroid.Constants;
+import es.ugr.swad.swadroid.R;
+import es.ugr.swad.swadroid.gui.MenuActivity;
+
+public class ManageLocation extends MenuActivity {
+    /**
+     * Messages tag name for Logcat
+     */
+    private static final String TAG = Constants.APP_TAG + " Manage location";
+
+    /**
+     * Code width
+     */
+    private static final int CODE_WIDTH = 250;
+
+    /**
+     * Code height
+     */
+    private static final int CODE_HEIGHT = 250;
+
+    /**
+     * Barcode Encoder
+     */
+    private static BarcodeEncoder barcodeEncoder;
+
+    /* (non-Javadoc)
+     * @see es.ugr.swad.swadroid.modules.Module#onCreate(android.os.Bundle)
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.navigation);
+
+        setTitle(R.string.manageLocation);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        barcodeEncoder = new BarcodeEncoder();
+    }
+
+    /* (non-Javadoc)
+     * @see es.ugr.swad.swadroid.modules.Module#onStart()
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
+
 }
