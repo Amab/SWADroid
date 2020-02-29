13515df678bc9533bd4c389319dcd789d309c7ff
JaviBL8 <javibl8@correo.ugr.es>
2020-02-29 19:13:02 +0100

    Logic to calculate distance

--- SWADroid/src/main/java/es/ugr/swad/swadroid/modules/location/ManageLocation.java
+++ SWADroid/src/main/java/es/ugr/swad/swadroid/modules/location/ManageLocation.java
@@ -1,14 +1,26 @@
 package es.ugr.swad.swadroid.modules.location;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
 import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import es.ugr.swad.swadroid.Constants;
 import es.ugr.swad.swadroid.R;
@@ -50,7 +62,22 @@ public class ManageLocation extends MenuActivity {
      * Allow to share location
      */
     private static Boolean allowSharingLocation=false;
-
+    /**
+     * Update location
+     */
+    private static Button updateLocation;
+    /**
+     * Manage wifi properties
+     */
+    private WifiManager wifiManager;
+    /**
+     * Arraylisto to store SSID of available networks
+     */
+    private ArrayList<String> availableNetworks = new ArrayList<String>();
+    /**
+     * List to store ScanResults of available networks
+     */
+    private List<ScanResult> results;
     /* (non-Javadoc)
      * @see es.ugr.swad.swadroid.modules.Module#onCreate(android.os.Bundle)
      */
@@ -62,12 +89,6 @@ public class ManageLocation extends MenuActivity {
         setTitle(R.string.manageLocation);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         barcodeEncoder = new BarcodeEncoder();
-
-        history = findViewById(R.id.location_history_data);
-        if(history.getText().toString().matches(""))
-        {
-            history.setText("No data found");
-        }
     }
 
     /* (non-Javadoc)
@@ -105,12 +126,50 @@ public class ManageLocation extends MenuActivity {
                startActivity(intent);
            }
         });
+
+        //Show history by default
+        history = findViewById(R.id.location_history_data);
+        if(history.getText().toString().matches(""))
+        {
+            history.setText("No data found");
+        }
+
+        updateLocation = findViewById(R.id.updateLocation);
+        updateLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scanWifi();
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+    }
 
+    private void scanWifi() {
+        availableNetworks.clear();
+        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        wifiManager.startScan();
+    }
+
+    BroadcastReceiver wifiReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            results = wifiManager.getScanResults();
+            unregisterReceiver(this);
+
+            for (ScanResult scanResult : results){
+                String bssid = scanResult.BSSID.replace(":","");
+                double distance = (int)calculateDistance(scanResult.level,scanResult.frequency);
+            }
+        }
+    };
+
+    private double calculateDistance(double levelInDb, double freqInMHz)    {
+        double exp = (27.55 - (20 * Math.log10(freqInMHz)) + Math.abs(levelInDb)) / 20.0;
+        return Math.pow(10.0, exp);
     }
 
 
