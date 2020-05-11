fbe30f4a51a048382e21868ee67aa05c80cba079
JaviBL8 <javibl8@correo.ugr.es>
2020-05-11 21:20:55 +0200

    Location history showing up in activity

---
+++ SWADroid/src/main/java/es/ugr/swad/swadroid/modules/location/GetLocation.java
@@ -0,0 +1,137 @@
+package es.ugr.swad.swadroid.modules.location;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.ksoap2.serialization.SoapObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+import javax.xml.transform.Result;
+
+import es.ugr.swad.swadroid.Constants;
+import es.ugr.swad.swadroid.model.Location;
+import es.ugr.swad.swadroid.modules.login.Login;
+import es.ugr.swad.swadroid.webservices.IWebserviceClient;
+import es.ugr.swad.swadroid.webservices.SOAPClient;
+
+public class GetLocation extends AsyncTask<Void, Void, Void> {
+    /**
+     * Physical address of AP
+     */
+    private String mac;
+    /**
+     * Location related to mac
+     */
+    private Location location;
+    /**
+     * Messages tag name for Logcat
+     */
+    private static final String TAG = Constants.APP_TAG + " Get location";
+
+    private static String METHOD_NAME = "";
+    private static IWebserviceClient webserviceClient;
+    private static Object result;
+
+    GetLocation(String mac){
+        super();
+        this.mac = mac;
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            location = getLocation(mac);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+        getValue();
+    }
+
+    private Location getLocation(String mac) throws Exception {
+        Log.d(TAG, "Get location from MAC address");
+
+        METHOD_NAME= "getLocations";
+
+        createRequest(SOAPClient.CLIENT_TYPE);
+        addParam("wsKey", Login.getLoggedUser().getWsKey());
+        addParam("MAC", mac);
+        return sendRequest(Location.class, true);
+    }
+
+    /**
+     * Creates webservice request.
+     */
+    private static void createRequest(String clientType) {
+        if(webserviceClient == null) {
+            if(clientType.equals(SOAPClient.CLIENT_TYPE)) {
+                webserviceClient = new SOAPClient();
+            }
+
+        }
+
+        webserviceClient.setMETHOD_NAME(METHOD_NAME);
+        webserviceClient.createRequest();
+    }
+
+    /**
+     * Adds a parameter to webservice request.
+     *
+     * @param param Parameter name.
+     * @param value Parameter value.
+     */
+    private static void addParam(String param, Object value) {
+        webserviceClient.addParam(param, value);
+    }
+
+    /**
+     * Sends a SOAP request to the specified webservice in METHOD_NAME class
+     * constant of the webservice client.
+     *
+     * @param cl     Class to be mapped
+     * @param simple Flag for select simple or complex response
+     * @throws Exception
+     */
+    protected Location sendRequest(Class<?> cl, boolean simple) throws Exception {
+        ((SOAPClient) webserviceClient).sendRequest(cl, simple);
+        result = webserviceClient.getResult();
+
+        if (result!=null) {
+
+            SoapObject soap = (SoapObject) result;
+            Integer institutionCode = Integer.valueOf(soap.getProperty("institutionCode").toString());
+            String institutionShortName = soap.getProperty("institutionShortName").toString();
+            String institutionFullName = soap.getProperty("institutionShortName").toString();
+            Integer centerCode = Integer.valueOf(soap.getProperty("centerCode").toString());
+            String centerShortName = soap.getProperty("centerShortName").toString();
+            String centerFullName = soap.getProperty("centerFullName").toString();
+            Integer buildingCode = Integer.valueOf(soap.getProperty("buildingCode").toString());
+            String buildingShortName = soap.getProperty("buildingShortName").toString();
+            String buildingFullName = soap.getProperty("buildingFullName").toString();
+            Integer floor = Integer.valueOf(soap.getProperty("floor").toString());
+            Integer roomCode = Integer.valueOf(soap.getProperty("roomCode").toString());
+            String roomShortName = soap.getProperty("roomShortName").toString();
+            String roomFullName = soap.getProperty("roomFullName").toString();
+
+            location = new Location(institutionCode, institutionShortName, institutionFullName,
+                    centerCode, centerShortName, centerFullName, buildingCode, buildingShortName, buildingFullName,
+                    floor, roomCode, roomShortName, roomFullName);
+
+        }
+        return location;
+    }
+
+    /**
+     * Get location
+     */
+    public Location getValue() {
+        return location;
+    }
+}
--- SWADroid/src/main/java/es/ugr/swad/swadroid/modules/location/ManageLocation.java
+++ SWADroid/src/main/java/es/ugr/swad/swadroid/modules/location/ManageLocation.java
@@ -55,7 +55,7 @@ public class ManageLocation extends MenuActivity {
     /**
      * Arraylist to store SSID of available networks
      */
-    private List<Pair> availableNetworks = new ArrayList<>();
+    private ArrayList<String> availableNetworks = new ArrayList<>();
     /**
      * Adapter of LocationDistance array
      */
@@ -65,9 +65,6 @@ public class ManageLocation extends MenuActivity {
      */
     private ArrayList<UserFilter> arrayReceivers;
     private ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-    private static String METHOD_NAME = "";
-    private static IWebserviceClient webserviceClient;
-    private static Object result;
     /* (non-Javadoc)
      * @see es.ugr.swad.swadroid.modules.Module#onCreate(android.os.Bundle)
      */
@@ -185,15 +182,21 @@ public class ManageLocation extends MenuActivity {
             unregisterReceiver(this);
             if (results.size() > 0) {
                 for (ScanResult scanResult : results) {
-                    String bssid = scanResult.BSSID.replace(":", "");
-                    double distance = (int) calculateDistance(scanResult.level, scanResult.frequency);
                     try {
-                        AsyncLocation asyncLocation = new AsyncLocation(bssid, distance);
-                        asyncLocation.execute();
+                        String bssid = scanResult.BSSID.replace(":", "");
+                        double distance = (int) calculateDistance(scanResult.level, scanResult.frequency);
+                        bssid = "F07F0667D5FF";
+                        GetLocation getLocation = new GetLocation(bssid);
+                        getLocation.execute().get();
+                        Location location = getLocation.getValue();
+                        availableNetworks.add("Estás a " + distance + "m de " +
+                                location.getRoomFullName() + " ( " + location.getCenterShortName() + " " + location.getInstitutionShortName() + " )");
+                        adapter.notifyDataSetChanged();
                     } catch (Exception e) {
                         Log.d(TAG, "EXCEPCION AL OBTENER LA LOCALIZACIÓN");
                         e.printStackTrace();
                     }
+
                 }
             }
         }
@@ -204,132 +207,4 @@ public class ManageLocation extends MenuActivity {
         return Math.pow(10.0, exp);
     }
 
-    /*
-    private void addLocations(String bssid, double distance) {
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        String uri = url + "ap/" + bssid;
-        Log.d(TAG, "ADD LOCATIONS");
-        JsonObjectRequest jsObjectRequest = new JsonObjectRequest(Request.Method.GET, uri, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
-                     availableNetworks.add("Estás a " + (int)distance + " m de " + response.getString(("es")));
-                     adapter.notifyDataSetChanged();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        },
-            new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Log.d("TAG", "Error JSON: " + error.getMessage());
-                }
-            });
-        requestQueue.add(jsObjectRequest);
-    }*/
-    /**
-     * Creates webservice request.
-     */
-    private static void createRequest(String clientType) {
-        if(webserviceClient == null) {
-            if(clientType.equals(SOAPClient.CLIENT_TYPE)) {
-                webserviceClient = new SOAPClient();
-            }
-
-        }
-
-        webserviceClient.setMETHOD_NAME(METHOD_NAME);
-        webserviceClient.createRequest();
-    }
-
-    /**
-     * Adds a parameter to webservice request.
-     *
-     * @param param Parameter name.
-     * @param value Parameter value.
-     */
-    private static void addParam(String param, Object value) {
-        webserviceClient.addParam(param, value);
-    }
-
-    /**
-     * Sends a SOAP request to the specified webservice in METHOD_NAME class
-     * constant of the webservice client.
-     *
-     * @param cl     Class to be mapped
-     * @param simple Flag for select simple or complex response
-     * @throws Exception
-     */
-    protected List<Location> sendRequest(Class<?> cl, boolean simple) throws Exception {
-        ((SOAPClient) webserviceClient).sendRequest(cl, simple);
-        result = webserviceClient.getResult();
-
-        List<Location> locationList = new ArrayList<>();
-        if (result!=null) {
-
-            ArrayList<?> res = new ArrayList<>((Vector<?>) result);
-            SoapObject soap = (SoapObject) res.get(1);
-            int numLocations = soap.getPropertyCount();
-
-            for (int i = 0; i < numLocations; i++) {
-                SoapObject pii = (SoapObject) soap.getProperty(i);
-                Integer institutionCode = Integer.valueOf(pii.getProperty("institutionCode").toString());
-                String institutionShortName = pii.getProperty("institutionShortName").toString();
-                String institutionFullName = pii.getProperty("institutionShortName").toString();
-                Integer centerCode = Integer.valueOf(pii.getProperty("centerCode").toString());
-                String centerShortName = pii.getProperty("centerShortName").toString();
-                String centerFullName = pii.getProperty("centerFullName").toString();
-                Integer buildingCode = Integer.valueOf(pii.getProperty("buildingCode").toString());
-                String buildingShortName = pii.getProperty("buildingShortName").toString();
-                String buildingFullName = pii.getProperty("buildingFullName").toString();
-                Integer floor = Integer.valueOf(pii.getProperty("floor").toString());
-                Integer roomCode = Integer.valueOf(pii.getProperty("roomCode").toString());
-                String roomShortName = pii.getProperty("roomShortName").toString();
-                String roomFullName = pii.getProperty("roomFullName").toString();
-
-                locationList.add(new Location(institutionCode, institutionShortName, institutionFullName,
-                        centerCode, centerShortName, centerFullName, buildingCode, buildingShortName, buildingFullName,
-                        floor, roomCode, roomShortName, roomFullName));
-            }
-        }
-        return locationList;
-    }
-
-    private class AsyncLocation extends AsyncTask<String, Void, String> {
-
-        private String mac;
-        private Double distance;
-
-        AsyncLocation(String mac, Double distance){
-            super();
-            this.mac = mac;
-            this.distance = distance;
-        }
-
-        @Override
-        protected String doInBackground(String... strings) {
-            try {
-                for (Location l : getLocation(mac)) {
-                    Pair<Location, Double> locationInfo = new Pair<>(l, distance);
-                    availableNetworks.add(locationInfo);
-                    adapter.notifyDataSetChanged();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-    }
-
-    private List<Location> getLocation(String mac) throws Exception {
-        Log.d(TAG, "Get location from MAC address");
-
-        METHOD_NAME= "getLocations";
-
-        createRequest(SOAPClient.CLIENT_TYPE);
-        addParam("wsKey", Login.getLoggedUser().getWsKey());
-        addParam("MAC", mac);
-        return sendRequest(Object.class, false);
-    }
 }
